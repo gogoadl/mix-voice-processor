990e8591640309320e87ba06ebe3e28153d95a60
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-12-11 20:27:18 +0900

    feat : 영상 HLS 포맷 트랜스코딩 기능 추가

--- backend/processor/src/main/java/com/hw/mixvoice/processor/FFMPEGProcessor.java
+++ backend/processor/src/main/java/com/hw/mixvoice/processor/FFMPEGProcessor.java
@@ -9,7 +9,41 @@ import java.io.IOException;
 
 
 public class FFMPEGProcessor {
+    public static final String UPLOAD_DIR = "\\video";
+    public static final String TS_PATH = "\\ts";
+    public void videoHlsMake() throws IOException {
+        String filePath = "D:\\GitRepository_HW\\web\\mix-voice\\backend\\server\\src\\main\\resources\\static\\";
+        String fileName = "KakaoTalk_20230301_205552507";
+        String fileExtension = ".mp4";
+        FFmpeg ffmpeg = new FFmpeg("D:\\libaraies\\ffmpeg-6.1-full_build\\bin\\ffmpeg");
+        FFprobe ffprobe = new FFprobe("D:\\libaraies\\ffmpeg-6.1-full_build\\bin\\ffprobe");
 
+        FFmpegBuilder builder = new FFmpegBuilder()
+                //.overrideOutputFiles(true) // 오버라이드 여부
+                .setInput(filePath + fileName + fileExtension) // 동영상파일
+                .addOutput(filePath + TS_PATH + "\\" + fileName + ".m3u8") // 썸네일 경로
+                .addExtraArgs("-profile:v", "baseline") //
+                .addExtraArgs("-level", "3.0") //
+                .addExtraArgs("-start_number", "0") //
+                .addExtraArgs("-hls_time", "10") //
+                .addExtraArgs("-hls_list_size", "0") //
+                .addExtraArgs("-f", "hls") //
+                .done();
+        FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
+        executor.createJob(builder).run();
+
+        // 이미지 파일 생성
+        FFmpegBuilder builderThumbNail = new FFmpegBuilder()
+                .overrideOutputFiles(true) // 오버라이드 여부
+                .setInput(filePath + fileName + fileExtension) // 동영상파일
+                .addExtraArgs("-ss", "00:00:03") // 썸네일 추출 시작점
+                .addOutput(filePath + UPLOAD_DIR + "\\" + fileName + ".png") // 썸네일 경로
+                .setFrames(1) // 프레임 수
+                .done();
+        FFmpegExecutor executorThumbNail = new FFmpegExecutor(ffmpeg, ffprobe);
+        executorThumbNail.createJob(builderThumbNail).run();
+
+    }
     public void encode() throws IOException {
         FFmpeg ffmpeg = new FFmpeg("D:\\libaraies\\ffmpeg-6.1-full_build\\bin\\ffmpeg");
         FFprobe ffprobe = new FFprobe("D:\\libaraies\\ffmpeg-6.1-full_build\\bin\\ffprobe");
--- backend/processor/src/main/java/com/hw/mixvoice/processor/Main.java
+++ backend/processor/src/main/java/com/hw/mixvoice/processor/Main.java
@@ -5,7 +5,8 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         FFMPEGProcessor ffmpegProcessor = new FFMPEGProcessor();
-        ffmpegProcessor.encode();
+//        ffmpegProcessor.encode();
+        ffmpegProcessor.videoHlsMake();
         System.out.println("Hello world!");
     }
 }
