5e9dd8512ba452bee583f9c4e1f7c6bc1afb71ca
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-12-12 02:05:24 +0900

    fix : 테스트용 소스코드 api 경로 수정

--- backend/server/src/main/java/com/hw/mixvoice/domain/user/User.java
+++ backend/server/src/main/java/com/hw/mixvoice/domain/user/User.java
@@ -15,6 +15,7 @@ public class User extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
 
     @Column(nullable = false)
