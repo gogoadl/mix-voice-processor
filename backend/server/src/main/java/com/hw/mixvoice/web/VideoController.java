bda9c3c4674bec5c4f351ea22024e868c8cf7437
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-12-12 02:05:36 +0900

    fix : 테스트용 api 경로 수정

--- backend/server/src/main/java/com/hw/mixvoice/web/VideoController.java
+++ backend/server/src/main/java/com/hw/mixvoice/web/VideoController.java
@@ -3,11 +3,17 @@ package com.hw.mixvoice.web;
 import com.hw.mixvoice.service.VideoService;
 import com.hw.mixvoice.util.FileUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.core.io.support.ResourceRegion;
 import org.springframework.http.*;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Optional;
@@ -16,37 +22,23 @@ import java.util.Optional;
 @RequestMapping("/video")
 @RequiredArgsConstructor
 @RestController
+@Slf4j
 public class VideoController {
     private final VideoService videoService;
 
-    @GetMapping("/save")
+    @GetMapping("/test/save")
     public void saveTest() throws IOException {
         videoService.saveFile(FileUtil.getMultipartFile("D:\\GitRepository_HW\\web\\mix-voice\\backend\\server\\src\\main\\resources\\static\\offspring.wav"));
     }
-    @GetMapping(path = "/downloadtest", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    public ResponseEntity<ResourceRegion> downloadTest(@RequestHeader HttpHeaders httpHeaders) throws FileNotFoundException, IOException {
-        UrlResource resource = videoService.downloadFile("offspring.wav");
+    @ResponseBody
+    @GetMapping("/test/download")
+    public ResponseEntity<Resource> downloadTest(@RequestHeader HttpHeaders httpHeaders) throws FileNotFoundException, IOException {
+        UrlResource resource = videoService.downloadFile("ts/KakaoTalk_20230301_205552507.m3u8");
+        InputStreamResource resource2 = new InputStreamResource((resource.getInputStream()));
 
-        ResourceRegion resourceRegion;
-
-        final long chunkSize = 1000000L;
-        long contentLength = resource.contentLength();
-
-        Optional<HttpRange> optional = httpHeaders.getRange().stream().findFirst();
-        HttpRange httpRange;
-        if (optional.isPresent()) {
-            httpRange = optional.get();
-            long start = httpRange.getRangeStart(contentLength);
-            long end = httpRange.getRangeEnd(contentLength);
-            long rangeLength = Long.min(chunkSize, end - start + 1);
-            resourceRegion = new ResourceRegion(resource, start, rangeLength);
-        } else {
-            long rangeLength = Long.min(chunkSize, contentLength);
-            resourceRegion = new ResourceRegion(resource, 0, rangeLength);
-        }
-
-        return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
-                .contentType(MediaTypeFactory.getMediaType(resource).orElse(MediaType.APPLICATION_OCTET_STREAM))
-                .body(resourceRegion);
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType("application/x-mpegURL"))
+                .body(resource2);
     }
+
 }
