5f551786a4d458b4b07af525446066cf5362e4fb
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-12-13 17:59:28 +0900

    Fix : processor 모듈 삭제 및 server 모듈로 통합

--- backend/server/src/main/java/com/hw/mixvoice/util/FFMPEGProcessor.java
+++ backend/server/src/main/java/com/hw/mixvoice/util/FFMPEGProcessor.java
@@ -1,27 +1,32 @@
-package com.hw.mixvoice.processor;
+package com.hw.mixvoice.util;
 
 import net.bramp.ffmpeg.FFmpeg;
 import net.bramp.ffmpeg.FFmpegExecutor;
 import net.bramp.ffmpeg.FFprobe;
 import net.bramp.ffmpeg.builder.FFmpegBuilder;
+import net.bramp.ffmpeg.builder.FFmpegOutputBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 
+@Component
 public class FFMPEGProcessor {
+    FFmpeg ffmpeg;
+    FFprobe ffprobe;
+    public FFMPEGProcessor() throws IOException {
+        ffmpeg = new FFmpeg("D:\\libaraies\\ffmpeg-6.1-full_build\\bin\\ffmpeg");
+        ffprobe = new FFprobe("D:\\libaraies\\ffmpeg-6.1-full_build\\bin\\ffprobe");
+    }
     public static final String UPLOAD_DIR = "\\video";
     public static final String TS_PATH = "\\ts";
-    public void videoHlsMake() throws IOException {
-        String filePath = "D:\\GitRepository_HW\\web\\mix-voice\\backend\\server\\src\\main\\resources\\static\\";
-        String fileName = "KakaoTalk_20230301_205552507";
-        String fileExtension = ".mp4";
-        FFmpeg ffmpeg = new FFmpeg("D:\\libaraies\\ffmpeg-6.1-full_build\\bin\\ffmpeg");
-        FFprobe ffprobe = new FFprobe("D:\\libaraies\\ffmpeg-6.1-full_build\\bin\\ffprobe");
-
+    public String videoHlsMake(String filePath) throws IOException {
+        String m3u8Path = filePath + ".m3u8";
         FFmpegBuilder builder = new FFmpegBuilder()
                 //.overrideOutputFiles(true) // 오버라이드 여부
-                .setInput(filePath + fileName + fileExtension) // 동영상파일
-                .addOutput(filePath + TS_PATH + "\\" + fileName + ".m3u8") // 썸네일 경로
+                .setInput(filePath) // 동영상파일
+                .addOutput(m3u8Path) // 썸네일 경로
                 .addExtraArgs("-profile:v", "baseline") //
                 .addExtraArgs("-level", "3.0") //
                 .addExtraArgs("-start_number", "0") //
@@ -31,19 +36,21 @@ public class FFMPEGProcessor {
                 .done();
         FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
         executor.createJob(builder).run();
-
-        // 이미지 파일 생성
-        FFmpegBuilder builderThumbNail = new FFmpegBuilder()
-                .overrideOutputFiles(true) // 오버라이드 여부
-                .setInput(filePath + fileName + fileExtension) // 동영상파일
-                .addExtraArgs("-ss", "00:00:03") // 썸네일 추출 시작점
-                .addOutput(filePath + UPLOAD_DIR + "\\" + fileName + ".png") // 썸네일 경로
-                .setFrames(1) // 프레임 수
-                .done();
-        FFmpegExecutor executorThumbNail = new FFmpegExecutor(ffmpeg, ffprobe);
-        executorThumbNail.createJob(builderThumbNail).run();
-
+        return m3u8Path;
     }
+//    public void videoThumbnailMake() throws IOException {
+//        // 이미지 파일 생성
+//        FFmpegBuilder builderThumbNail = new FFmpegBuilder()
+//                .overrideOutputFiles(true) // 오버라이드 여부
+//                .setInput(filePath + fileName + fileExtension) // 동영상파일
+//                .addExtraArgs("-ss", "00:00:03") // 썸네일 추출 시작점
+//                .addOutput(filePath + UPLOAD_DIR + "\\" + fileName + ".png") // 썸네일 경로
+//                .setFrames(1) // 프레임 수
+//                .done();
+//        FFmpegExecutor executorThumbNail = new FFmpegExecutor(ffmpeg, ffprobe);
+//        executorThumbNail.createJob(builderThumbNail).run();
+//    }
+
     public void encode() throws IOException {
         FFmpeg ffmpeg = new FFmpeg("D:\\libaraies\\ffmpeg-6.1-full_build\\bin\\ffmpeg");
         FFprobe ffprobe = new FFprobe("D:\\libaraies\\ffmpeg-6.1-full_build\\bin\\ffprobe");
