12d7e506b84a9014504c7af1bdb77a1b70d1c658
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-12-25 18:38:28 +0900

    Feat : 1080, 720, 480 화질 별 hls 영상 생성 기능 추가
    아래 블로그 참고하여 작성https://velog.io/@haerong22/%EC%98%81%EC%83%81-%EC%8A%A4%ED%8A%B8%EB%A6%AC%EB%B0%8D-8.-%EC%8A%A4%ED%8A%B8%EB%A6%AC%EB%B0%8DHLS-%EC%9E%90%EB%8F%99-%ED%99%94%EC%A7%88-%EC%A1%B0%EC%A0%88

--- backend/server/src/main/java/com/hw/mixvoice/util/FFMPEGProcessor.java
+++ backend/server/src/main/java/com/hw/mixvoice/util/FFMPEGProcessor.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 
 
@@ -24,31 +25,72 @@ public class FFMPEGProcessor {
         ffprobe = new FFprobe(ffprobePath);
     }
 
-    public void videoHlsMake(String filePath) throws IOException {
+    public void videoHlsMake(String filePath, String fileName) throws IOException {
         String m3u8Path = filePath + ".m3u8";
         String thumbnailPath = filePath + ".png";
+
+        File _1080 = new File(filePath, "1080");
+        File _720 = new File(filePath, "720");
+        File _480 = new File(filePath, "480");
+
+        if (!_1080.exists()) _1080.mkdirs();
+        if (!_720.exists()) _720.mkdirs();
+        if (!_480.exists()) _480.mkdirs();
+
         FFmpegBuilder builder = new FFmpegBuilder()
                 //.overrideOutputFiles(true) // 오버라이드 여부
-                .setInput(filePath) // 동영상파일
-                .addOutput(m3u8Path)
-                .addExtraArgs("-profile:v", "baseline") //
-                .addExtraArgs("-level", "3.0") //
+                .setInput(filePath+fileName) // 동영상파일
+                .addOutput(filePath+"/%v/playlist.m3u8")
+//                .addExtraArgs("-profile:v", "baseline") //
+//                .addExtraArgs("-level", "3.0") //
                 .addExtraArgs("-start_number", "0") //
                 .addExtraArgs("-hls_time", "10") //
                 .addExtraArgs("-hls_list_size", "0") //
                 .addExtraArgs("-f", "hls") //
+                .addExtraArgs("-hls_segment_filename", filePath + "/%v/output_%03d.ts") // ts 파일 이름 (ex: output_000.ts)
+                .addExtraArgs("-master_pl_name", "master.m3u8") // 마스터 재생 파일
+
+                .addExtraArgs("-map", "0:v")
+                .addExtraArgs("-map", "0:v")
+                .addExtraArgs("-map", "0:v")
+                .addExtraArgs("-var_stream_map", "v:0,name:1080 v:1,name:720 v:2,name:480") // 출력 매핑
+
+                // 1080 화질 옵션
+                .addExtraArgs("-b:v:0", "5000k")
+                .addExtraArgs("-maxrate:v:0", "5000k")
+                .addExtraArgs("-bufsize:v:0", "10000k")
+                .addExtraArgs("-s:v:0", "1920x1080")
+                .addExtraArgs("-crf:v:0", "15")
+                .addExtraArgs("-b:a:0", "128k")
+
+                // 720 화질 옵션
+                .addExtraArgs("-b:v:1", "2500k")
+                .addExtraArgs("-maxrate:v:1", "2500k")
+                .addExtraArgs("-bufsize:v:1", "5000k")
+                .addExtraArgs("-s:v:1", "1280x720")
+                .addExtraArgs("-crf:v:1", "22")
+                .addExtraArgs("-b:a:1", "96k")
+
+                // 480 화질 옵션
+                .addExtraArgs("-b:v:2", "1000k")
+                .addExtraArgs("-maxrate:v:2", "1000k")
+                .addExtraArgs("-bufsize:v:2", "2000k")
+                .addExtraArgs("-s:v:2", "854x480")
+                .addExtraArgs("-crf:v:2", "28")
+                .addExtraArgs("-b:a:2", "64k")
                 .done();
         FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
         executor.createJob(builder).run();
 
         FFmpegBuilder builderThumbNail = new FFmpegBuilder()
                 .overrideOutputFiles(true) // 오버라이드 여부
-                .setInput(filePath) // 동영상파일
+                .setInput(filePath+fileName) // 동영상파일
                 .addExtraArgs("-ss", "00:00:03") // 썸네일 추출 시작점
                 .addOutput(thumbnailPath) // 썸네일 경로
                 .setFrames(1) // 프레임 수
                 .done();
         FFmpegExecutor executorThumbNail = new FFmpegExecutor(ffmpeg, ffprobe);
         executorThumbNail.createJob(builderThumbNail).run();
+
     }
 }
